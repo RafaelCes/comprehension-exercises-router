1bc7a6544190fa8f13c6ed33823d28674ba78289
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-06-07 15:42:23 -0300

    exercicio 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import About from './components/About';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
+        <Route path='/about' component={ About } />
         <Route path='/' component={ Home } />
       </BrowserRouter>
     );
