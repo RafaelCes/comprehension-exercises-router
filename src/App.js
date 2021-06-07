96e3e4fff1e77eb69d9f953e803620aa8e73e1c2
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-06-07 16:42:04 -0300

    exercicio 12

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
+import StricAccess from './components/StrictAccess';
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 
 class App extends Component {
@@ -17,6 +18,9 @@ class App extends Component {
           </li>
           <li>
             <Link to='/users'>Users</Link>
+          </li>
+          <li>
+            <Link to='/strict-access'>Strict Access</Link>
           </li>
         </ul>
         <Switch>
