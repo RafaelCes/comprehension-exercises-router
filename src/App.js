0cf8d935cfb56761b0b672a329b4eb151684769c
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-06-07 15:51:32 -0300

    exercicio 6

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/users'>Users</Link>
+          </li>
+        </ul>
         <Route path='/about' component={ About } />
         <Route path='/users' component={ Users } />
         <Route exact path='/' component={ Home } />
